cae25ff3899f6df9f8dcc1894d3670059aff1c38
hernandessn <hernandessalesneves7@gmail.com>
2026-05-06 15:25:49 -0300

    feat(board): add BoardException for custom error handling

--- Board/Board.cs
+++ Board/Board.cs
@@ -16,10 +16,43 @@
         {
             return _pieces[line, column];
         }
+
+        public Piece piece(Position pos)
+        {
+            return _pieces[pos.Line, pos.Column];
+        }
+
+        public bool ExistPiece(Position pos)
+        {
+            ValidatePosition(pos);
+            return piece(pos) != null;
+        }
+
         public void ToPutPiece(Piece p, Position pos)
         {
+            if (ExistPiece(pos))
+            {
+                throw new BoardException("There is already a piece in that position!");
+            }
             _pieces[pos.Line, pos.Column] = p;
             p.Position = pos;
+        }
+
+        public bool PositionValidation(Position pos)
+        {
+            if (pos.Line < 0 || pos.Line >= Lines || pos.Column < 0 || pos.Column >= Columns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidatePosition(Position pos)
+        {
+            if (!PositionValidation(pos))
+            {
+                throw new BoardException("Invalid position!");
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,14 +8,22 @@ namespace chess_console
     {
         static void Main(string[] args)
         {
-            Board board = new Board(8, 8);
+            try
+            {
+                Board board = new Board(8, 8);
 
-            board.ToPutPiece(new Tower(board, Color.Black), new Position(0,0 ));
-            board.ToPutPiece(new Tower(board, Color.Black), new Position(1, 3));
-            board.ToPutPiece(new King(board, Color.Black), new Position(2, 4));
+                board.ToPutPiece(new Tower(board, Color.Black), new Position(0, 0));
+                board.ToPutPiece(new Tower(board, Color.Black), new Position(1, 9));
+                board.ToPutPiece(new King(board, Color.Black), new Position(0, 2));
 
-            Screen.PrintBoard(board);
+                Screen.PrintBoard(board);
 
+                Console.ReadLine();
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
             Console.ReadLine();
         }
     }
